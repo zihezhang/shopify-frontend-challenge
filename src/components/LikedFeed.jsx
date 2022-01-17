a4fc74677dcabf620ed1374fda2c23a87d716039
Zihe Zhang <51214897+zihezhang@users.noreply.github.com>
2022-01-17 14:18:11 -0500

    liked feed

--- src/components/LikedFeed.jsx
+++ src/components/LikedFeed.jsx
@@ -16,26 +16,9 @@ const FeedContainer = styled.div`
 `;
 
 function Feed() {
-  const todayDate = new Date();
-  const days = 86400000;
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
-  const [likedImages, setLikedImages] = useState();
-  console.log(todayDate.getMonth() + 1);
-  const [{ month, year }, setDate] = useState({
-    month: { ...todayDate.getMonth() },
-    year: todayDate.getFullYear(),
-  });
-  const [selectedDates, setSelectedDates] = useState({
-    start: new Date(todayDate - 5 * days),
-    end: todayDate,
-  });
-
-  const handleMonthChange = useCallback(
-    (month, year) => setDate({ month, year }),
-    []
-  );
 
   const [play, setPlay] = React.useState(false);
   const url = play
@@ -46,15 +29,19 @@ function Feed() {
   // this useEffect will run once
   // similar to componentDidMount()
   useEffect(() => {
-    const start = moment(todayDate).subtract(7, "days").format("YYYY-MM-DD");
-    const end = moment(todayDate).format("YYYY-MM-DD");
-    fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API}&start_date=${start}&end_date=${end}&thumbs=true`
+    const likedPosts = getLiked();
+    console.log(likedPosts);
+    Promise.all(
+      likedPosts.map((date) =>
+        fetch(
+          `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API}&date=${date}&thumbs=true`
+        )
+      )
     )
-      .then((res) => res.json())
+      .then((res) => Promise.all(res.map((response) => response.json())))
       .then(
         (result) => {
-          console.log(result, selectedDates);
+          console.log(result);
 
           setIsLoaded(true);
           setItems(result.reverse());
@@ -68,7 +55,7 @@ function Feed() {
           setError(error);
         }
       );
-  }, [selectedDates]);
+  }, []);
 
   if (error) {
     return <FeedContainer>Error: {error.message}</FeedContainer>;
